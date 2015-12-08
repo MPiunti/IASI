18765c319eaef6eb64d0030aca8d30d00d846ba3
Michele Piunti <pompeo@gmail.com>
2015-12-08 09:40:59 +0100

    refinements

--- SpringWS-Producer/src/main/java/com/concretepage/StudentUtility.java
+++ SpringWS-Producer/src/main/java/com/concretepage/StudentUtility.java
@@ -11,19 +11,24 @@ import com.concretepage.soap.Student;
 public class StudentUtility {
 	private Map<Integer,Student> studentMap = new HashMap<Integer,Student>();
 	public StudentUtility(){
+		
 		Student s1 = new Student();
 		s1.setStudentId(1);
-		s1.setName("Ram");
+		s1.setName("Michael Jordan");
 		s1.setAge(20);
-		s1.setClazz("ABC");
+		s1.setClazz("2-F");
 		studentMap.put(1, s1);
+		
+		
 		Student s2 = new Student();
 		s2.setStudentId(2);
-		s2.setName("Shyam");
+		s2.setName("Shyam Gestalt");
 		s2.setAge(22);
-		s2.setClazz("EFG");
+		s2.setClazz("4-G");
 		studentMap.put(2, s2);
 	}
+	
+	
 	public Student getStudent(int studentId){
 		return studentMap.get(studentId);
 	}
